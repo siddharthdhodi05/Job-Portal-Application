6e83290e308bbe27b0b0c4dea4d25f93fef9c920
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-10-21 21:31:38 +0530

    user.controller done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -98,3 +98,67 @@ export const login = async (req, res) => {
       });
   } catch (error) {}
 };
+
+export const logout = async (req, res) =>{
+  try {
+    return res.status(200).cookie("token", "",{maxAge:0}).json({
+      message: "logout successful",
+      success: true,
+    })
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, bio, skills } = req.body;
+        
+        //const file = req.file;
+        // cloudinary ayega idhar
+        
+        let skillsArray;
+        if(skills){
+            skillsArray = skills.split(",");
+        }
+        const userId = req.id; // middleware authentication
+        let user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not found.",
+                success: false
+            })
+        }
+        // updating data
+        if(fullname) user.fullname = fullname
+        if(email) user.email = email
+        if(phoneNumber)  user.phoneNumber = phoneNumber
+        if(bio) user.profile.bio = bio
+        if(skills) user.profile.skills = skillsArray
+      
+        // resume comes later here...
+        
+
+
+        await user.save();
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+
+        return res.status(200).json({
+            message:"Profile updated successfully.",
+            user,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
